d9455ce58087608a76bcefaa68221f834753028d
Fabio Martins <fabiovige@gmail.com>
2024-03-16 09:13:42 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>API Teste</h1>
+  <Profile />
+</template>
+
+<script>
+import Profile from './components/Profile.vue';
+
+export default {
+  components: {
+    Profile,
+  },
+};
+</script>
---
+++ src/components/Profile.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>{{ profile.name }}</h2>
+    <h4>{{ profile.bio }}</h4>
+    <img :src="profile.avatar_url" alt="Avatar" width="80" />
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'Profile',
+    data() {
+        return {
+            profile: {}
+        };
+    },
+
+  methods: {
+    loadProfile() {
+      axios.get('https://api.github.com/users/fabiovige')
+        .then(response => {
+          this.profile = response.data;
+          console.log(this.profile);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+    this.loadProfile();
+  }
+};
+</script>
